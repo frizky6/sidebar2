3134c63c67e93c2178554f2d6744352bead4ebad
frizky6 <40273466+frizky6@users.noreply.github.com>
2018-06-15 15:11:54 +0700

    Update Sticky Sidebar Post.js

--- Sticky Sidebar Post.js
+++ Sticky Sidebar Post.js
@@ -1,6 +1,6 @@
 $('#sidebarleft, #sidebarright').stickySidebar({
 	containerSelector: '.container',
 	innerWrapperClass: 'sidebar__inner',
-	topSpacing: 5,
-	bottomSpacing: 5
+	topSpacing: 15,
+	bottomSpacing: 15
 });
